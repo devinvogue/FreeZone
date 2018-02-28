9ddba00516525a1a219892c3d94d8d8141de3b52
Maxwell <geek@max-dex.com>
2018-02-28 12:59:39 +0000

    final work

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 function register() {
-
    var userAge = prompt("Enter your age");
 
   if(userAge >= 18){
-    window.location.href="index.html";
+    window.location.href="register.html";
   }else{
-    document.write('You are not eligible to register');
+  	alert('You are not eligible to register');
   }
 }
